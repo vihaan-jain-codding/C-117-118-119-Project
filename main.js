13a50c94431f6226d2f76be92184d9aad546cdaa
vihaan-jain-codding <94980425+vihaan-jain-codding@users.noreply.github.com>
2023-12-03 00:33:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,48 @@ timer_counter = 0;
 timer_check = "";
 answer_holder = "";
 score = 0;
+drawn_sketch = "";
 
 random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
 console.log(quick_draw_data_set[random_number]);
 sketch = quick_draw_data_set[random_number];
 document.getElementById("skecth_to_be_drawn").innerHTML = "Sketch To Be Drawn: " + sketch;
+
+function setup(){
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}   
+
+function update_canvas(){
+    random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    console.log(quick_draw_data_set[random_number]);
+    sketch = quick_draw_data_set[random_number];
+    document.getElementById("skecth_to_be_drawn").innerHTML = "Sketch To Be Drawn: " + sketch;   
+}
+
+function draw(){
+    check_sketch();
+    if (drawn_sketch == sketch){
+        answer_holder = "set";
+        score++
+        document.getElementById("score").innerHTML = "Score: " + score;
+    }
+}
+
+function check_sketch(){
+    timer_counter++
+    document.getElementById("timer").innerHTML = "Timer: " + timer_counter
+    console.log(timer_counter);
+    if (timer_counter > 400){
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+
+    if(timer_check == "completed" || answer_holder == "set"){
+        timer_check = ""
+        answer_holder = ""
+        update_canvas();
+    }
+
+}
